b3cdf4cd2fe3152e48bf1b4cf5e3d501aae3de95
Luis Peña <raptor_luis@hotmail.com>
2024-10-15 21:19:15 -0300

    add ahref

--- 15_10_2024/Ejemplo/Ejemplo/settings.py
+++ 15_10_2024/Ejemplo/Ejemplo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Ejemplo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates','templates/blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- 15_10_2024/Ejemplo/blog/views.py
+++ 15_10_2024/Ejemplo/blog/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
- 
+from django.template import loader
  
 def blog(request):
-  return HttpResponse("PAGINA DEL BLOG")
+    template = loader.get_template('blog.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
