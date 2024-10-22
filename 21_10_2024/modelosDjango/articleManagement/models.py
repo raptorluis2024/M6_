cc439dd431cda28419fca0e6b04b38492c85bcac
Luis Peña <raptor_luis@hotmail.com>
2024-10-21 22:12:33 -0300

    add validation

--- 21_10_2024/modelosDjango/articleManagement/models.py
+++ 21_10_2024/modelosDjango/articleManagement/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
 # Create your models here.
 class Article(models.Model):
     name = models.CharField(max_length = 30)
     address = models.CharField(max_length = 30)
     price = models.IntegerField()
     category = models.CharField(max_length = 30)
+    
+    def clean(self):
+        if not self.name:
+            raise ValidationError("El nombre es obligatorio.")
+        if self.price <= 0:
+            raise ValidationError("El precio debe ser mayor que 0.")
+
+    def save(self, *args, **kwargs):
+        #self.full_clean()
+        super().save(*args, **kwargs)
+        #super(Article,self).save(*args, **kwargs)
